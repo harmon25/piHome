15016a5c76cf07aa510bb27ae33a2edfc48423b9
harmon <harmon@debian8>
2015-04-09 01:30:57 -0400

    Events working using queues

--- pulserpi.py
+++ pulserpi.py
@@ -6,131 +6,120 @@
 # @Last Modified time: 2015-04-06 00:36:40
 from time import sleep
 from math import sin, radians
+from multiprocessing import Process, Queue
 
+import pigpio
+
+COMMON_ANODE=True
 PosSinWave = lambda amp, ang, freq: amp + (amp * sin(radians(ang) * freq))
+CYCLES=2
 
-class PulserPi(object):
+if COMMON_ANODE == True:
+	MAX_BRIGHTNESS = 0
+	MIN_BRIGHTNESS = 255
+	OFF = 1
+else:
+	MAX_BRIGHTNESS = 255
+	MIN_BRIGHTNESS = 0
+	OFF = 0
+
+def pulse_off(pi, pins=[]):
+	if isinstance(pins,int):
+		pins = [pins]
+	dutyList = []
+	for p in pins:
+		dutyList.append(int(pi.get_PWM_dutycycle(p)))
+	avg = sum(dutyList)/len(dutyList)
+	for i in range(int(avg), MIN_BRIGHTNESS, 5):
+		for p in pins:
+			pi.set_PWM_dutycycle(p, i)
+		sleep(0.04)
+	for p in pins:
+		pi.write(p, OFF)
+
+def pulse_on(pi, pins=[], c=2):
+	if isinstance(pins,int):
+		pins = [pins]
+	for i in range(0, c*360, 5):
+		for p in pins:
+			pi.set_PWM_dutycycle(p, int(PosSinWave(125, i, 1)))
+		sleep(0.05)
+
+def rgb(pi, pins, c=3):
+	freq = [0.5,1,2]
+	for i in range(0, c*360, 5):
+		for idx, p in enumerate(pins):
+			pi.set_PWM_dutycycle(p, int(PosSinWave(125, i, freq[idx])))
+		sleep(0.05)
+
+def pulser(pi,pins,queue):
+	qu = queue
+	w = pins    #111
+	r = w[0] 		#100
+	g = w[1] 		#010
+	b = w[2] 		#001
+	v, y, c = [r,b], [r,g], [g,b] # 101,110, 011
+	while True:
+		if qu.empty():
+			pulse_on(pi, w)
+		else:
+			e = qu.get()
+			if e == "r":
+				pulse_off(pi, w)
+				pulse_on(pi, r)
+			elif e == "g":
+				pulse_off(pi, w)
+				pulse_on(pi, g)
+			elif e == "b":
+				pulse_off(pi, w)
+				pulse_on(pi, b)
+			elif e == "v":
+				pulse_off(pi, w)
+				pulse_on(pi, v)
+			elif e == "y":
+				pulse_off(pi, w)
+				pulse_on(pi, y)
+			elif e == "c":
+				pulse_off(pi, w)
+				pulse_on(pi, c)
+			elif e == "0":
+				pulse_off(pi, w)
+				break
+			else:
+				rgb(pi, w)
+				pulse_off(pi, w)
+
+def set_pins(r=22, g=23, b=24):
+		if not isinstance(r, int) or not isinstance(g, int) or not isinstance(b, int):
+			print("One of those is not an integer...")
+		else:
+			return [r,g,b]
+
+class notifyLight:
 	'''
 	Notification Light
 	'''
-	def __init__(self, pi):
-		self.HOST = ''
-		self.PORT0 = 5011
-		self.AUTHKEY = b'authkey'
-		self.PI = pi
-		self.PINS = [22, 23, 24]
-		self.ON = False
-		self.CYCLES = 2
-	def QueueClient(HOST, PORT, AUTHKEY):
-		from multiprocessing.managers import SyncManager
-		class QueueManager(SyncManager):
-			pass
-		QueueManager.register('get_queue')
-		QueueManager.register('get_name')
-		QueueManager.register('get_description')
-		manager = QueueManager(address = (HOST, PORT), authkey = AUTHKEY)
-		manager.connect() # This starts the connected client
+	def __init__(self, pi=""):
+		p = set_pins()
+		self.RGB = p
+		self.pi =  pigpio.pi(pi)
+		self.queue = Queue()
+	
+	def change_pins(self,r, g, b):
+		self.RGB = set_pins(r,g,b)
 
-		return manager
-	def set_pins(self, red=22, green=23, blue=24):
-		if not isinstance(red, int) or not isinstance(green, int) or not isinstance(blue, int):
-			print("One of those is not an integer...")
-		else:
-			self.PINS[0]=red
-			self.PINS[1]=green
-			self.PINS[2]=blue
-			print("Red pin set to: {}\nGreen pin set to: {}\nBlue pin set to: {}".format(red,green,blue))
-	def check_setup(self):
-		from pigpio import pi
-		if None in self.PINS:
-			msg = "Must first set_pins"
-			return [False, msg]
-		elif not isinstance(self.PI, pi):
-			msg = "Set PIGPIO object as PiLight.PI - pi = pigpio.pi()\nPiLightClass.PI = pi"
-			return [False, msg]
-		else:
-			msg = "Setup!"
-			return [True, msg]
-	def white(self):
-		for i in range(0, self.CYCLES*360, 5):
-			for p in self.PINS:
-				self.PI.set_PWM_dutycycle(p, int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for p in self.PINS:
-			for x in range(self.PI.get_PWM_dutycycle(p), 255 ,5):
-				self.PI.set_PWM_dutycycle(p,x)
-		sleep(0.05)
-	def red(self):
-		for i in range(0, self.CYCLES*360, 5):
-			self.PI.set_PWM_dutycycle(self.PINS[0], int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(self.PINS[0]), 255 ,5):
-			self.PI.set_PWM_dutycycle(self.PINS[0],x)
-		sleep(0.05)
-	def blue(self):
-		for i in range(0, self.CYCLES*360, 5):
-			self.PI.set_PWM_dutycycle(self.PINS[2], int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(self.PINS[2]), 255 ,5):
-			self.PI.set_PWM_dutycycle(self.PINS[2],x)
-			sleep(0.05)
-	def green(self):
-		for i in range(0, self.CYCLES*360, 5):
-			self.PI.set_PWM_dutycycle(self.PINS[1], int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(self.PINS[1]), 255 ,5):
-			self.PI.set_PWM_dutycycle(self.PINS[1],x)
-			sleep(0.05)
-	def cyan(self):
-		CPINS = [self.PINS[1],self.PINS[2]]
-		for i in range(0, self.CYCLES*360, 5):
-			for p in CPINS:
-				self.PI.set_PWM_dutycycle(p, int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(p), 255 ,5):
-			for p in CPINS:
-				self.PI.set_PWM_dutycycle(p,x)
-		sleep(0.05)
-	def violet(self):
-		VPINS = [self.PINS[0],self.PINS[2]]
-		for i in range(0, self.CYCLES*360, 5):
-			for p in VPINS:
-				self.PI.set_PWM_dutycycle(p, int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(p), 255 ,5):
-			for p in VPINS:
-				self.PI.set_PWM_dutycycle(p,x)
-		sleep(0.05)
-	def yellow(self):
-		YPINS = [self.PINS[0],self.PINS[1]]
-		for i in range(0, self.CYCLES*360, 5):
-			for p in YPINS:
-				self.PI.set_PWM_dutycycle(p, int(PosSinWave(125, i, 1)))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(p), 255 ,5):
-			for p in YPINS:
-				self.PI.set_PWM_dutycycle(p,x)
-		sleep(0.05)
-	def rgb(self):
-		freq = [0.5,1,2]
-		for i in range(0, self.CYCLES*360, 5):
-			for idx, p in enumerate(self.PINS):
-				self.PI.set_PWM_dutycycle(p, int(PosSinWave(125, i, freq[idx])))
-			sleep(0.05)
-		for x in range(self.PI.get_PWM_dutycycle(p), 255 ,5):
-			for p in self.PINS:
-				self.PI.set_PWM_dutycycle(p,x)
-		sleep(0.05)
+	def event(self, e=None):
+		self.queue.put(e)
 
-	def main(self):
-		self.white()
-		self.red()
-		self.green()
-		self.blue()
-		self.cyan()
-		self.violet()
-		self.yellow()
-		self.rgb()
+	def stop(self):
+		self.queue.put("000")
 
-	if __name__ == '__main__':
-		main()
+	def run(self):
+		p = Process(target=pulser, args=(self.pi,self.RGB,self.queue,), daemon=True)
+		p.start()
+
+		
+		
+		
+
+
